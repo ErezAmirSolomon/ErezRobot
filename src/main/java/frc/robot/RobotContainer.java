bca99d0e65e0cf1028122bbb0c44640aa21af28c
neatteamsoftware <neatteamsoftware@gmail.com>
2023-07-16 19:27:57 +0300

    src: robotcontainer: Add objects for Oshoot and ShootBalls and set running conditions

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,11 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.StorageConstants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.RotateUntilLimit;
+import frc.robot.commands.ShootBalls;
 import frc.robot.subsystems.MecanumDriveTrain;
+import frc.robot.subsystems.OShoot;
 import frc.robot.subsystems.Storage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -17,12 +19,15 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private MecanumDriveTrain m_MecanumDriveTrain = new MecanumDriveTrain();
   private Storage m_Storage = new Storage();
+  private OShoot m_OShoot = new OShoot();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
 
   private RotateUntilLimit m_RotateUntilLimit = new RotateUntilLimit(m_Storage);
+
+  private ShootBalls m_ShootBalls = new ShootBalls(m_OShoot);
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -33,11 +38,16 @@ public class RobotContainer {
     m_MecanumDriveTrain
         .setDefaultCommand(new RunCommand(() -> m_MecanumDriveTrain.move(m_driverController), m_MecanumDriveTrain));
 
-    m_Storage.setDefaultCommand(new RunCommand(() -> m_Storage.setMotorSpeed(0), m_Storage));
+    m_Storage
+        .setDefaultCommand(new RunCommand(() -> m_Storage.setMotorSpeed(0), m_Storage));
+
+    m_OShoot
+        .setDefaultCommand(new RunCommand(() -> m_OShoot.setShooterMotorSpeed(ShooterConstants.kShooterMotorControllerSpeed), m_OShoot));
   }
 
   private void configureBindings() {
-    m_driverController.x().onTrue(m_RotateUntilLimit);
+    m_driverController.a().onTrue(m_RotateUntilLimit);
+    m_driverController.x().onTrue(m_ShootBalls);
   }
 
   /**
