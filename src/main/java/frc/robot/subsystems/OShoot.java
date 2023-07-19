55d17e7ae603c1eac64c35a23c2b027332cb61e3
neatteamsoftware <neatteamsoftware@gmail.com>
2023-07-19 18:18:41 +0300

    src: commands: add encoder and bang bang controller

--- src/main/java/frc/robot/subsystems/OShoot.java
+++ src/main/java/frc/robot/subsystems/OShoot.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
@@ -11,18 +13,26 @@ import frc.robot.Constants.ShooterConstants;
 
 public class OShoot extends SubsystemBase {
   private VictorSP m_shooterMotorController;
+  private Encoder m_encoder;
+
+  private BangBangController m_controller;
 
   /** Creates a new OShoot. */
   public OShoot() {
     m_shooterMotorController = new VictorSP(ShooterConstants.kShooterMotorControllerPort);
+    m_encoder = new Encoder(ShooterConstants.kEncoderPortA, ShooterConstants.kEncoderPortB);
+  }
+
+  public void setShooterMotorSpeed(int motorSpeed) {
+    m_shooterMotorController.set(motorSpeed);
+  }
+
+  public double getRPM() {
+    return m_encoder.getRate() / ShooterConstants.kEncoderResolution * 60;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-  
-  public void setShooterMotorSpeed(int motorSpeed) {
-    m_shooterMotorController.set(motorSpeed);
   }
 }
